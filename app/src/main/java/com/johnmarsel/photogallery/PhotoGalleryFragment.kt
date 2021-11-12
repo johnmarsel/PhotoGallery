ae98222908308253ed1e126c964115e9a9c834d8
johnmarsel <vlad.gorbachyov@gmail.com>
2021-11-12 17:44:51 +0300

    Added Flickr(HTTP) repository

--- app/src/main/java/com/johnmarsel/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/johnmarsel/photogallery/PhotoGalleryFragment.kt
@@ -1,16 +1,31 @@
 package com.johnmarsel.photogallery
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.LiveData
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+
+private const val TAG = "PhotoGalleryFragment"
 
 class PhotoGalleryFragment : Fragment() {
 
     private lateinit var photoRecyclerView: RecyclerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val flickrLiveData: LiveData<String> = FlickrFetchr().fetchContents()
+        flickrLiveData.observe(
+            this,
+            { responseString ->
+                Log.d(TAG, "Response received: $responseString")
+            })
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
